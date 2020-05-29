a622850d0f4ddb4fb20c85cb6e75a073aa027620
Mohammad Mohsen-Pour <mohammad.mohsenpour1@gmail.com>
2020-05-29 07:07:55 -0400

    Updates

---
+++ precache-manifest.98062817297192690a1b4069fa1d1fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cc2fa32c934bba409aec18ee2ba844",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "04ff90f4f6e8cac14b3b",
+    "url": "/monsters-roledex/static/css/main.abc341d2.chunk.css"
+  },
+  {
+    "revision": "d6e4ecd5ab8b0e82d993",
+    "url": "/monsters-roledex/static/js/2.d5e9deb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.d5e9deb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ff90f4f6e8cac14b3b",
+    "url": "/monsters-roledex/static/js/main.f20aa14a.chunk.js"
+  },
+  {
+    "revision": "0e9bc445ccb1118a3b9d",
+    "url": "/monsters-roledex/static/js/runtime-main.632dcb50.js"
+  }
+]);
